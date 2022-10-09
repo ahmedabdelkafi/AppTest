6ca51ffefcd311bd12a75253e39504ef24344578
ahmedabdelkafi <ahmed.abdelkafi@esprit.tn>
2022-10-09 23:56:55 +0100

    update all files

--- app/src/main/java/com/example/apptest/MainActivity.java
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -5,6 +5,7 @@ import static android.widget.Toast.LENGTH_LONG;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,69 +17,48 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 //1ere Etape: DECLARATION
     private Button next ;
-    private EditText password ;
-    private EditText username ;
+    private EditText password,username ;
     private ImageView pass ;
     private TextView register ;
-
+    private SharedPreferences mPreferences;
+    public static final String sharedPrefFile = "tn.esprit.myapplication";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         String a = "";
+        String a = "";
 
 
-    //2eme Etape: IDENTIFICATION
+        //2eme Etape: IDENTIFICATION
         next = findViewById(R.id.button);
         password = findViewById(R.id.editTextTextPassword3);
+        username = findViewById(R.id.editTextTextPersonName);
         pass = findViewById(R.id.imageView3);
         register = findViewById(R.id.textView8);
-
-
         //recuperer text de l edittext
         a = password.getText().toString();
+        //instance of sharedpreferences
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        username.setText(mPreferences.getString("login", ""));
+        password.setText(mPreferences.getString("password", ""));
+
+        if(mPreferences.contains("login")||mPreferences.contains("pwd")){
+
+            username.setText(mPreferences.getString("login",""));
+            password.setText(mPreferences.getString("pwd",""));
+
+        }
+
+        next.setOnClickListener(e -> {
+
+            SharedPreferences.Editor ed = mPreferences.edit();
+            ed.putString("login", username.getText().toString());
+            ed.putString("pwd", password.getText().toString());
+            ed.apply();
+        });
 
 
-        View.OnClickListener l = new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                //action to do
-                //Dans put Extra (tag,valeur)
-                Intent i = new Intent(getApplicationContext(), MainActivity2.class).putExtra("aaa",password.getText().toString());
-                startActivity(i);
-
-            }
-        };
-        next.setOnClickListener(l);
-
-        View.OnClickListener e = new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                //action to do
-                //Dans put Extra (tag,valeur)
-                Intent i = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivity(i);
-
-            }
-        };
-        register.setOnClickListener(e);
-
-
-        View.OnClickListener p = new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Toast.makeText(getApplicationContext(), password.getText().toString(), LENGTH_LONG).show();
-
-            }
-        };
-        pass.setOnClickListener(p);
     }
 }
--- app/src/main/java/com/example/apptest/MainActivity2.java
+++ app/src/main/java/com/example/apptest/MainActivity2.java
@@ -3,19 +3,25 @@ package com.example.apptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity2 extends AppCompatActivity {
     private TextView malek ;
+    private SharedPreferences mPreferences;
+    private Button logout ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         malek = findViewById(R.id.textView2);
-
+        logout= findViewById(R.id.button8);
         String a ;
         Intent iin= getIntent();
         Bundle b = iin.getExtras();
@@ -24,5 +30,19 @@ public class MainActivity2 extends AppCompatActivity {
         {
             String j =(String) b.get("aaa");
             malek.setText(j);
-        }    }
+        }
+        View.OnClickListener l = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+                preferencesEditor.clear();
+                preferencesEditor.apply();
+                finish();
+            }
+        };
+        logout.setOnClickListener(l);
+    }
+
+
 }
