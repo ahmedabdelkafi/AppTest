fb47661df25371c08a43da6bc7fe09150d590305
ghofrane99 <ghofrane.chaouachi@esprit.tn>
2022-10-20 21:01:04 +0100

    juste khadamet fragment

--- app/src/main/java/com/example/apptest/MainActivity2.java
+++ app/src/main/java/com/example/apptest/MainActivity2.java
@@ -1,14 +1,59 @@
 package com.example.apptest;
+import androidx.annotation.NonNull;
+import com.example.apptest.*;
+import com.example.apptest.databinding.ActivityMain2Binding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity2 extends AppCompatActivity {
+import java.io.*;
+
+
+
+
+public class MainActivity2 extends AppCompatActivity   {
+
+  ActivityMain2Binding binding ;
+
+
+   BottomNavigationView bottomNavigationView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new demand());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case R.id.demand:
+                    replaceFragment(new demand());
+                    break;
+                case R.id.offer:
+                    replaceFragment(new offer());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new settings());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment,fragment);
+        fragmentTransaction.commit();
+
     }
 }
---
+++ app/src/main/java/com/example/apptest/offer.java
@@ -0,0 +1,29 @@
+package com.example.apptest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class offer extends Fragment {
+
+
+    public offer() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_offer, container, false);
+    }
+}
